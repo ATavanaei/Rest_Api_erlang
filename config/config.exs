b8538cd0843de5fe8f12219da135a8b4ccd5fc2e
a.r.tavanaei <ali_t775@hotmail.com>
2020-08-24 17:39:00 +0430

    Add files via upload
    http server base on elixer and erlang

---
+++ config/config.exs
@@ -0,0 +1,13 @@
+# You can configure your application as:
+#
+#     config :http_server, key: :value
+#
+# and access this configuration in your application as:
+#
+#     Application.get_env(:http_server, :key)
+#
+# You can also configure a third-party app:
+#
+#     config :logger, level: :info
+#
+#     import_config "#{Mix.env()}.exs"
